d81e0166d7740e88a26fb713eb0ecbfc1701d1b9
Reeba <146749647+Reeba20@users.noreply.github.com>
2025-08-31 20:50:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// const socket = io('http://localhost:3000')
-const socket = io("https://chatting-application-blgr.onrender.com");
+const socket = io('http://localhost:3000')
+// const socket = io("https://chatting-application-blgr.onrender.com");
 
 const messageContainer = document.getElementById('message-container')
 const messageInput =document.getElementById('message-input')
